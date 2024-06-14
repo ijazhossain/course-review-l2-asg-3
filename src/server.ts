5d6215a5ab72c7dfe33f9f8d3369492e698db0ea
ijazhossain <ijaz.hossain35@gmail.com>
2024-06-14 09:24:05 +0600

    Fixing an issues

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import app from './app';
 import config from './app/config';
-
 import mongoose from 'mongoose';
+import { Server } from 'http';
+let server: Server;
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
-    app.listen(config.port, () => {
+    server = app.listen(config.port, () => {
       console.log(`Example app listening on port ${config.port}`);
     });
   } catch (error) {
